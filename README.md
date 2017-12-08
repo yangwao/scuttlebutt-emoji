b32c2701b9926f8be275cf696f29db259efca24b
Matej Nemček <ybdaba@gmail.com>
2017-12-08 15:28:41 +0100

    wip encode & decode

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 # scuttlebutt-emoji
 
+### encode
+```
+⫸ node index.js @NaDXehMSIgk08W5RXZJ0p+7m+19iIWEuAtD7FRESJX8=.ed25519 encode
+🌵🎬🌳🍬🐄🐬💧👏🎊🍒🚪👓💣🍞👫💐💻🐄👑🐔💣💾📘👦👣📘📖👰👂🍒👫🚗🐸🍰🍟🍬👦🐈💻🚗🎊🐔🐄🍞💡🐗🐬💵📚💐💐📖👰
+```
+
+### decode
+```
+⫸ node index.js 🌵🎬🌳🍬🐄🐬💧👏🎊🍒🚪👓💣🍞👫💐💻🐄👑🐔💣💾📘👦👣📘📖👰👂🍒👫🚗🐸🍰🍟🍬👦🐈💻🚗🎊🐔🐄🍞💡🐗🐬💵📚💐💐📖👰
+@NaDXehMSIgk08W5RXZJ0p+7m+19iIWEuAtD7FRESJX8=.ed25519
+```
+
 
 inspired by [emojilock](https://github.com/maxogden/emojilock)
+
+powered by [pfraze/base-emoji](https://www.npmjs.com/package/base-emoji)
--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 // ssb://@NaDXehMSIgk08W5RXZJ0p+7m+19iIWEuAtD7FRESJX8=.ed25519
 const baseEmoji = require('base-emoji')
 const buf = new Buffer('@NaDXehMSIgk08W5RXZJ0p+7m+19iIWEuAtD7FRESJX8=.ed25519', 'ascii')
+// console.log(baseEmoji.fromUnicode(baseEmoji.toUnicode(buf)).toString('utf8'));
+const id = process.argv[2]
+const op = process.argv[3]
 
-console.log(baseEmoji.toUnicode(buf));
-
-console.log(baseEmoji.fromUnicode(baseEmoji.toUnicode(buf)).toString('utf8'));
+if (!id) {
+  console.log(`
+    @NaDXehMSIgk08W5RXZJ0p+7m+19iIWEuAtD7FRESJX8=.ed25519
+    ${baseEmoji.toUnicode(buf)}`)
+} else if (id.length > 0 && op === 'encode') {
+  const inputBuf = new Buffer(process.argv[2], 'ascii')
+  console.log(baseEmoji.toUnicode(inputBuf))
+} else if (id.length > 0) {
+  const inputBuf = process.argv[2]
+  console.log(baseEmoji.fromUnicode(inputBuf).toString('utf8'))
+}
