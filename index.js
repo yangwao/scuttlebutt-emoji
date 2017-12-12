f115c8727f1e98d8fc46f83cc496c35796e2d7ad
Matej Nemček <ybdaba@gmail.com>
2017-12-12 08:14:13 +0100

    instance not req

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // ssb://@NaDXehMSIgk08W5RXZJ0p+7m+19iIWEuAtD7FRESJX8=.ed25519
 const baseEmoji = require('base-emoji')
-const buf = new Buffer.from('@NaDXehMSIgk08W5RXZJ0p+7m+19iIWEuAtD7FRESJX8=.ed25519', 'ascii')
+const buf = Buffer.from('@NaDXehMSIgk08W5RXZJ0p+7m+19iIWEuAtD7FRESJX8=.ed25519', 'ascii')
 console.log(baseEmoji.fromUnicode(baseEmoji.toUnicode(buf)).toString('utf8'))
 const id = process.argv[2]
 const op = process.argv[3]
@@ -10,7 +10,7 @@ if (!id) {
     @NaDXehMSIgk08W5RXZJ0p+7m+19iIWEuAtD7FRESJX8=.ed25519
     ${baseEmoji.toUnicode(buf)}`)
 } else if (id.length > 0 && op === 'encode') {
-  const inputBuf = new Buffer.from(process.argv[2], 'ascii')
+  const inputBuf = Buffer.from(process.argv[2], 'ascii')
   console.log(baseEmoji.toUnicode(inputBuf))
 } else if (id.length > 0) {
   const inputBuf = process.argv[2]
